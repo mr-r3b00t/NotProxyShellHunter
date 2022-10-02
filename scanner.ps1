45f3796423f26dd914d59a6516d6b3907af8ea51
Daniel Card <dan.card@xservus.com>
2022-10-02 10:27:03 +0100

    Update scanner.ps1

--- scanner.ps1
+++ scanner.ps1
@@ -1,7 +1,11 @@
 $target = "mail.local"
+$checkdns = Resolve-DnsName $target -ErrorAction SilentlyContinue
+
+if($checkdns){
+Write-host "Running autodiscover test..." -ForegroundColor Cyan
 try
 {
-$webtest = Invoke-WebRequest -uri "https://$target/autodiscover"
+$webtest1 = Invoke-WebRequest -uri "https://$target/autodiscover"
 }
 catch
 {
@@ -10,20 +14,39 @@ $Failure = $_.Exception.Response
 $Failure.Headers.tostring()
 }
 
+
+
+Write-host "Running autodiscover SSRF test..." -ForegroundColor Cyan
 try
 {
-$webtest = invoke-webrequest -uri "https://$target/autodiscover/autodiscover.json?scanner4329@pwnstar.local/owa/&Email=autodiscover/autodiscover.json?a@foo.var&Protocol=XYZ&FooProtocol=Powershell"
+write-host "testing site..." -ForegroundColor Gray
+$webtest2 = invoke-webrequest -uri "https://$target/autodiscover/autodiscover.json?scanner4329@pwnstar.local/owa/&Email=autodiscover/autodiscover.json?a@foo.var&Protocol=XYZ&FooProtocol=Powershell"
 }
 catch
 {
-$Failure = $_.Exception.Response
-$Failure.Headers.tostring()
-$Failure.Headers.tostring() | findstr /I "X-OWA-Version"
-$Failure.Headers.tostring() | findstr /I "X-FEServer"
-if($Failure.Headers -contains "X-FEServer")
-{
-write-host "Poentially vulnerable to CVE-2022-40140 & CVE-2022-41082" -ForegroundColor Red
+    write-host "Caught" -ForegroundColor Red
+    $Failure = $_.Exception.Response
+    $Failure.Headers.tostring()
+    $Failure.Headers.tostring() | findstr /I "X-OWA-Version"
+    $Failure.Headers.tostring() | findstr /I "X-FEServer"
+    if($Failure.Headers -contains "X-FEServer")
+        {
+        write-host "Poentially vulnerable to CVE-2022-40140 & CVE-2022-41082" -ForegroundColor Red
+        }
 }
 
 
+    if($webtest2.StatusCode -eq 200)
+    {
+    write-host "Authenticated to Server" -ForegroundColor Green
+    if($webtest2.Headers.Keys -contains "X-FEServer")
+            {
+            write-host "Poentially vulnerable to CVE-2022-40140 & CVE-2022-41082" -ForegroundColor Red
+            }
+        }
+
+}
+else
+{
+write-host "DNS does not resolve" -ForegroundColor Red
 }
